392228b268f587b2897fff1edede1165756b4565
Thirukumaran-T <ThirukumaranT.dev@gmail.com>
2025-10-04 10:05:41 +0530

    initial commit

---
+++ src/test/java/com/example/Menu/Management/MenuManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Menu.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
